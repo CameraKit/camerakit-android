fba3ed4ee123c3619ffac4623f16a0d07dbb0ce2
Dylan McIntyre <dwillmc@gmail.com>
2017-03-10 12:54:35 -0500

    Finish basic tap to focus

--- camerakit/src/main/api16/com/flurgle/camerakit/Camera1.java
+++ camerakit/src/main/api16/com/flurgle/camerakit/Camera1.java
@@ -5,7 +5,6 @@ import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
@@ -19,7 +18,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import static android.content.ContentValues.TAG;
 import static com.flurgle.camerakit.CameraKit.Constants.FLASH_OFF;
 import static com.flurgle.camerakit.CameraKit.Constants.FOCUS_CONTINUOUS;
 import static com.flurgle.camerakit.CameraKit.Constants.FOCUS_OFF;
@@ -30,11 +28,9 @@ import static com.flurgle.camerakit.CameraKit.Constants.METHOD_STILL;
 @SuppressWarnings("deprecation")
 public class Camera1 extends CameraImpl {
 
-
     private static final int FOCUS_AREA_SIZE_DEFAULT = 300;
-    private static final int FOCUS_METERING_AREA_WEIGHT_DEFAULT = 800;
+    private static final int FOCUS_METERING_AREA_WEIGHT_DEFAULT = 1000;
 
-    private View view;
     private int mCameraId;
     private Camera mCamera;
     private Camera.Parameters mCameraParameters;
@@ -46,8 +42,6 @@ public class Camera1 extends CameraImpl {
     private Camera.AutoFocusCallback mAutofocusCallback;
 
     private int mDisplayOrientation;
-    private int mFocusAreaSize = 0;
-    private int mFocusMeteringAreaWeight = 0;
 
     @Facing
     private int mFacing;
@@ -66,7 +60,6 @@ public class Camera1 extends CameraImpl {
 
     Camera1(CameraListener callback, PreviewImpl preview) {
         super(callback, preview);
-        this.view = preview.getView();
         preview.setCallback(new PreviewImpl.Callback() {
             @Override
             public void onSurfaceChanged() {
@@ -152,6 +145,7 @@ public class Camera1 extends CameraImpl {
         switch (focus) {
             case FOCUS_CONTINUOUS:
                 if (mCameraParameters != null) {
+                    detachFocusTapListener();
                     final List<String> modes = mCameraParameters.getSupportedFocusModes();
                     if (modes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
                         mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
@@ -162,12 +156,18 @@ public class Camera1 extends CameraImpl {
                 break;
 
             case FOCUS_TAP:
-                mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-                attachFocusTapListener();
+                if (mCameraParameters != null) {
+                    attachFocusTapListener();
+                    final List<String> modes = mCameraParameters.getSupportedFocusModes();
+                    if (modes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                        mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                    }
+                }
                 break;
 
             case FOCUS_OFF:
                 if (mCameraParameters != null) {
+                    detachFocusTapListener();
                     final List<String> modes = mCameraParameters.getSupportedFocusModes();
                     if (modes.contains(Camera.Parameters.FOCUS_MODE_FIXED)) {
                         mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_FIXED);
@@ -295,106 +295,7 @@ public class Camera1 extends CameraImpl {
         return mCamera != null;
     }
 
-    void setFocusAreaSize(int size) {
-        mFocusAreaSize = size;
-    }
-
-    /**
-     *  @link {@link android.hardware.Camera.Area#weight}
-     */
-    void setFocusMeteringAreaWeight(int weight) {
-        mFocusMeteringAreaWeight = weight;
-    }
-
-    void setTapToAutofocusListener(Camera.AutoFocusCallback callback) {
-        if (this.mFocus != FOCUS_TAP) {
-            throw new IllegalArgumentException("Please set the camera to FOCUS_TAP.");
-        }
-
-        this.mAutofocusCallback = callback;
-    }
-
-    private int getFocusAreaSize() {
-        if (mFocusAreaSize == 0) {
-            return FOCUS_AREA_SIZE_DEFAULT;
-        } else {
-            return mFocusAreaSize;
-        }
-    }
-
-    private int getFocusMeteringAreaWeight() {
-        if (mFocusMeteringAreaWeight == 0) {
-            return FOCUS_METERING_AREA_WEIGHT_DEFAULT;
-        } else {
-            return mFocusMeteringAreaWeight;
-        }
-    }
-
     // Internal:
-
-    private void attachFocusTapListener() {
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    if (mCamera != null ) {
-
-                        Camera.Parameters parameters = mCamera.getParameters();
-                        if (parameters.getMaxNumMeteringAreas() > 0){
-                            Log.i(TAG,"fancy !");
-                            Rect rect = calculateFocusArea(event.getX(), event.getY());
-
-                            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-                            List<Camera.Area> meteringAreas = new ArrayList<>();
-                            meteringAreas.add(new Camera.Area(rect, FOCUS_METERING_AREA_WEIGHT_DEFAULT));
-                            parameters.setFocusAreas(meteringAreas);
-
-                            mCamera.setParameters(parameters);
-                            mCamera.autoFocus(new Camera.AutoFocusCallback() {
-                                @Override
-                                public void onAutoFocus(boolean success, Camera camera) {
-                                    if (mAutofocusCallback != null) {
-                                        mAutofocusCallback.onAutoFocus(success, camera);
-                                    }
-                                }
-                            });
-                        } else {
-                            mCamera.autoFocus(new Camera.AutoFocusCallback() {
-                                @Override
-                                public void onAutoFocus(boolean success, Camera camera) {
-                                    if (mAutofocusCallback != null) {
-                                        mAutofocusCallback.onAutoFocus(success, camera);
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-                return true;
-            }
-        });
-    }
-
-    private Rect calculateFocusArea(float x, float y) {
-        int left = clamp(Float.valueOf((x / view.getWidth()) * 2000 - 1000).intValue(), getFocusAreaSize());
-        int top = clamp(Float.valueOf((y / view.getHeight()) * 2000 - 1000).intValue(), getFocusAreaSize());
-
-        return new Rect(left, top, left + getFocusAreaSize(), top + getFocusAreaSize());
-    }
-
-    private int clamp(int touchCoordinateInCameraReper, int focusAreaSize) {
-        int result;
-        if (Math.abs(touchCoordinateInCameraReper)+focusAreaSize/2>1000){
-            if (touchCoordinateInCameraReper>0){
-                result = 1000 - focusAreaSize/2;
-            } else {
-                result = -1000 + focusAreaSize/2;
-            }
-        } else{
-            result = touchCoordinateInCameraReper - focusAreaSize/2;
-        }
-        return result;
-    }
 
     private void openCamera() {
         if (mCamera != null) {
@@ -518,6 +419,103 @@ public class Camera1 extends CameraImpl {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    void setTapToAutofocusListener(Camera.AutoFocusCallback callback) {
+        if (this.mFocus != FOCUS_TAP) {
+            throw new IllegalArgumentException("Please set the camera to FOCUS_TAP.");
+        }
+
+        this.mAutofocusCallback = callback;
+    }
+
+    private int getFocusAreaSize() {
+        return FOCUS_AREA_SIZE_DEFAULT;
+    }
+
+    private int getFocusMeteringAreaWeight() {
+        return FOCUS_METERING_AREA_WEIGHT_DEFAULT;
+    }
+
+    private void detachFocusTapListener() {
+        mPreview.getView().setOnTouchListener(null);
+    }
+
+    private void attachFocusTapListener() {
+        mPreview.getView().setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (mCamera != null) {
+
+                        Camera.Parameters parameters = mCamera.getParameters();
+                        if (parameters.getMaxNumMeteringAreas() > 0) {
+                            Rect rect = calculateFocusArea(event.getX(), event.getY());
+
+                            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                            List<Camera.Area> meteringAreas = new ArrayList<>();
+                            meteringAreas.add(new Camera.Area(rect, getFocusMeteringAreaWeight()));
+                            parameters.setFocusAreas(meteringAreas);
+                            parameters.setMeteringAreas(meteringAreas);
+
+                            mCamera.setParameters(parameters);
+                            mCamera.autoFocus(new Camera.AutoFocusCallback() {
+                                @Override
+                                public void onAutoFocus(boolean success, Camera camera) {
+                                    camera.cancelAutoFocus();
+                                    Camera.Parameters params = camera.getParameters();
+                                    if (params.getFocusMode() != Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE) {
+                                        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                                        params.setFocusAreas(null);
+                                        params.setMeteringAreas(null);
+                                        camera.setParameters(params);
+                                    }
+
+                                    if (mAutofocusCallback != null) {
+                                        mAutofocusCallback.onAutoFocus(success, camera);
+                                    }
+                                }
+                            });
+                        } else {
+                            mCamera.autoFocus(new Camera.AutoFocusCallback() {
+                                @Override
+                                public void onAutoFocus(boolean success, Camera camera) {
+                                    if (mAutofocusCallback != null) {
+                                        mAutofocusCallback.onAutoFocus(success, camera);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private Rect calculateFocusArea(float x, float y) {
+        int centerX = clamp(Float.valueOf((x / mPreview.getView().getWidth()) * 2000 - 1000).intValue(), getFocusAreaSize());
+        int centerY = clamp(Float.valueOf((y / mPreview.getView().getHeight()) * 2000 - 1000).intValue(), getFocusAreaSize());
+        return new Rect(
+                centerX - getFocusAreaSize() / 2,
+                centerY - getFocusAreaSize() / 2,
+                centerX + getFocusAreaSize() / 2,
+                centerY + getFocusAreaSize() / 2
+        );
+    }
+
+    private int clamp(int touchCoordinateInCameraReper, int focusAreaSize) {
+        int result;
+        if (Math.abs(touchCoordinateInCameraReper) + focusAreaSize / 2 > 1000) {
+            if (touchCoordinateInCameraReper > 0) {
+                result = 1000 - focusAreaSize / 2;
+            } else {
+                result = -1000 + focusAreaSize / 2;
+            }
+        } else {
+            result = touchCoordinateInCameraReper - focusAreaSize / 2;
+        }
+        return result;
     }
 
 }
