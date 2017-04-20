c39cef3741d79267089cdba2dd7d3d6c2fd45015
android-dataticket <android-dataticket@users.noreply.github.com>
2017-04-20 12:53:16 -0700

    add pinch to zoom

--- camerakit/src/main/java/com/flurgle/camerakit/CameraView.java
+++ camerakit/src/main/java/com/flurgle/camerakit/CameraView.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
+import android.hardware.Camera;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
