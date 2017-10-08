f2a847888edce312a4adaebcc0cc0aed2ce03cfb
Dylan McIntyre <dwillmc@gmail.com>
2017-10-07 20:20:37 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+echo "Checking branch = $CIRCLE_BRANCH and $CIRCLE_TAG and deploying if necessary."
+
 if [[ "$CIRCLE_BRANCH" == "master" && "$CIRCLE_TAG" == *"v"* ]]; then
     ./gradlew clean build bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
 fi
