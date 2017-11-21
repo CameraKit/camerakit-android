91a969626915c12e4124329b286e952cce5d27c4
Caleb Lewis <caleb_dre@yahoo.com>
2017-11-20 23:46:22 -0500

    used a boolean to let the user know whether or not text detection actually started

--- camerakit/src/main/java/com/wonderkiln/camerakit/CameraView.java
+++ camerakit/src/main/java/com/wonderkiln/camerakit/CameraView.java
@@ -431,15 +431,19 @@ public class CameraView extends FrameLayout implements LifecycleObserver {
         captureImage(null);
     }
 
-    public void setTextDetectionListener(final CameraKitEventCallback<CameraKitTextDetect> callback) throws GooglePlayServicesUnavailableException {
+    public boolean setTextDetectionListener(final CameraKitEventCallback<CameraKitTextDetect> callback) throws GooglePlayServicesUnavailableException {
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getContext()).build();
         textRecognizer.setProcessor(new TextProcessor(mEventDispatcher, callback));
         int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getContext().getApplicationContext());
         if (code != ConnectionResult.SUCCESS) {
             throw new GooglePlayServicesUnavailableException();
         }
+
         if (textRecognizer.isOperational()) {
             mCameraImpl.setTextDetector(textRecognizer);
+            return true;
+        } else {
+            return false;
         }
     }
 
