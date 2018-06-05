56f23e9b787316d107dbc6a0045b0513e780acaf
emersoncloud <emerson.cloud@gmail.com>
2018-06-04 23:36:36 -0400

    Add additional CameraKitView unit tests

--- camerakit/src/test/java/com/camerakit/CameraKitViewTest.java
+++ camerakit/src/test/java/com/camerakit/CameraKitViewTest.java
@@ -28,10 +28,21 @@ public class CameraKitViewTest {
     private CameraKitView.GestureListener mockGestureListener;
 
     @Mock
+    private CameraKitView.CameraListener mockCameraListener;
+
+    @Mock
     private CameraKitView.ErrorListener mockErrorListener;
+
+    @Mock
+    private CameraKitView.PreviewListener mockPreviewListener;
 
     private static final int TEST_WIDTH = 2;
     private static final int TEST_HEIGHT = 2;
+    private static final int TEST_X = 2;
+    private static final int TEST_Y = 2;
+    private static final int TEST_DS = 2;
+    private static final int TEST_DSX = 2;
+    private static final int TEST_DSY = 2;
     private static final float TEST_ZOOM_FACTOR = 2.2f;
 
     private CameraKitView cameraKitView;
@@ -41,13 +52,15 @@ public class CameraKitViewTest {
     @Before
     public void setupTests() {
         MockitoAnnotations.initMocks(this);
-        when(mockContext.obtainStyledAttributes(mockAttributes, R.styleable.CameraKitView)).thenReturn(mockTypedArray);
+        when(mockContext.obtainStyledAttributes(mockAttributes, R.styleable.CameraKitView))
+                .thenReturn(mockTypedArray);
         cameraKitView = new CameraKitView(mockContext, mockAttributes);
         testSize = new CameraKitView.Size(TEST_WIDTH, TEST_HEIGHT);
 
         permissionsListener = new CameraKitView.PermissionsListener() {
             @Override
             public void onPermissionsSuccess() { }
+
             @Override
             public void onPermissionsFailure() { }
         };
@@ -55,7 +68,30 @@ public class CameraKitViewTest {
 
     /**
      * Tests of CameraKitView member functions
-     */
+     **/
+    @Test
+    public void onTapTest() {
+        cameraKitView.setGestureListener(mockGestureListener);
+        cameraKitView.onTap(TEST_X, TEST_Y);
+    }
+
+    @Test
+    public void onLongTapTest() {
+        cameraKitView.setGestureListener(mockGestureListener);
+        cameraKitView.onLongTap(TEST_X, TEST_Y);
+    }
+
+    @Test
+    public void onDoubleTapTest() {
+        cameraKitView.setGestureListener(mockGestureListener);
+        cameraKitView.onDoubleTap(TEST_X, TEST_Y);
+    }
+
+    @Test
+    public void onPinchTest() {
+        cameraKitView.setGestureListener(mockGestureListener);
+        cameraKitView.onPinch(TEST_DS, TEST_DSX, TEST_DSY);
+    }
 
     @Test
     public void onMeasureTest() {
@@ -133,6 +169,18 @@ public class CameraKitViewTest {
     public void setGestureListenerTest() {
         cameraKitView.setGestureListener(mockGestureListener);
         assertEquals(cameraKitView.getGestureListener(), mockGestureListener);
+    }
+
+    @Test
+    public void setCameraListener() {
+        cameraKitView.setCameraListener(mockCameraListener);
+        assertEquals(cameraKitView.getCameraListener(), mockCameraListener);
+    }
+
+    @Test
+    public void setPreviewListener(){
+        cameraKitView.setPreviewListener(mockPreviewListener);
+        assertEquals(cameraKitView.getPreviewListener(), mockPreviewListener);
     }
 
     @Test
